590c807fdd166ec61fcec87181228cc8ba7e234e
Anton Kindestam <antonki@kth.se>
2017-11-24 15:09:29 +0100

    boot.cmd first commit

---
+++ boot.cmd
@@ -0,0 +1,7 @@
+setenv machid 1029
+setenv bootargs earlyprintk console=/dev/ttyS0
+load mmc 0:1 0x43000000 boot/sun8i-h3-orangepi-pc.dtb
+load mmc 0:1 0x41000000 boot/zImage
+bootz 0x41000000 - 0x43000000
+#load mmc 0:1 0x45000000 boot/initramfs-sunxi-new
+#bootz 0x41000000 0x45000000 0x43000000
