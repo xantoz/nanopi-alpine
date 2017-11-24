da85ccc6a64e70092595e0972d28e959410ebb5f
Anton Kindestam <antonki@kth.se>
2017-11-24 21:39:28 +0100

    boot.cmd: give root dev to kernel

--- boot.cmd
+++ boot.cmd
@@ -1,5 +1,5 @@
 setenv machid 1029
-setenv bootargs earlyprintk console=/dev/ttyS0
+setenv bootargs earlyprintk console=/dev/ttyS0 root=/dev/mmcblk0p2
 load mmc 0:1 0x43000000 boot/sun8i-h3-orangepi-pc.dtb
 load mmc 0:1 0x41000000 boot/zImage
 bootz 0x41000000 - 0x43000000
