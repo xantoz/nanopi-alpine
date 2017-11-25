1bd9f035b033008ae31c486f4a68eef1b163da48
Anton Kindestam <antonki@kth.se>
2017-11-25 13:59:54 +0100

    boot.cmd: Fix DTB filename

--- boot.cmd
+++ boot.cmd
@@ -1,6 +1,6 @@
 setenv machid 1029
-setenv bootargs earlyprintk console=/dev/ttyS0 root=/dev/mmcblk0p2
-load mmc 0:1 0x43000000 sun8i-h3-orangepi-pc.dtb
+setenv bootargs earlyprintk console=/dev/ttyS0 root=/dev/mmcblk0p2w
+load mmc 0:1 0x43000000 sun8i-h3-nanopi-neo.dtb
 load mmc 0:1 0x41000000 zImage
 bootz 0x41000000 - 0x43000000
 #load mmc 0:1 0x45000000 boot/initramfs-sunxi-new
