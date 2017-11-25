daec4dd32fa06f2d61790acd208368d78b4b6d99
Anton Kindestam <antonki@kth.se>
2017-11-25 09:59:18 +0100

    boot.cmd: fix path to zImage and DTB

--- boot.cmd
+++ boot.cmd
@@ -1,7 +1,7 @@
 setenv machid 1029
 setenv bootargs earlyprintk console=/dev/ttyS0 root=/dev/mmcblk0p2
-load mmc 0:1 0x43000000 boot/sun8i-h3-orangepi-pc.dtb
-load mmc 0:1 0x41000000 boot/zImage
+load mmc 0:1 0x43000000 sun8i-h3-orangepi-pc.dtb
+load mmc 0:1 0x41000000 zImage
 bootz 0x41000000 - 0x43000000
 #load mmc 0:1 0x45000000 boot/initramfs-sunxi-new
 #bootz 0x41000000 0x45000000 0x43000000
