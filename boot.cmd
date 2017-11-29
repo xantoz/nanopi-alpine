fa09e8630008ca8b565e9f4490d0dcd6781873d3
Anton Kindestam <antonki@kth.se>
2017-11-29 16:33:29 +0100

    [boot.cmd] was missing noinitrd and rootwait in kernel parameters

--- boot.cmd
+++ boot.cmd
@@ -1,7 +1,7 @@
 setenv machid 1029
-setenv bootargs earlyprintk console=ttyS0,115200 root=/dev/mmcblk0p2
+setenv bootargs earlyprintk console=ttyS0,115200 root=/dev/mmcblk0p2 noinitrd rootwait
 load mmc 0:1 0x43000000 sun8i-h3-nanopi-neo.dtb
 load mmc 0:1 0x41000000 zImage
 bootz 0x41000000 - 0x43000000
-#load mmc 0:1 0x45000000 boot/initramfs-sunxi-new
+#load mmc 0:1 0x45000000 initrd
 #bootz 0x41000000 0x45000000 0x43000000
